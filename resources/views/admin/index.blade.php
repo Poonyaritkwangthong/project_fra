47711b4c5d8cd2d0e4d2919d47ab3f89b400a679
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2024-12-06 15:41:13 +0700

    Delete resources directory

--- resources/views/admin/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin')
-
-@section('title')
-    Admin index Page
-@endsection
-
-@section('content')
-<h1 class="text-pink-400 text-center text-3xl">Welcome Admin</h1>
-@endsection
